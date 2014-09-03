ab9bb32c737cc0bc0c5bf512e6542df4f212e08d
Malcolm Roberts <squidgyhead@gmail.com>
2014-09-03 17:22:28 +0200

    Add autocorrelation to stationary.py

--- stationary.py
+++ stationary.py
@@ -1,14 +1,51 @@
 #!/usr/bin/python -u
 
 import csv # to load the input file.
-import numpy # numerical methods such as FFTs.
+import numpy as np # numerical methods such as FFTs.
 import sys # to check for file existence, etc.
 import getopt # to pass command-line arguments
 import os.path #for file-existence checking
 
+def stationary_part(list):
+    # TODO: actually do some tests here.
+    return list
+
+def ypart(list):
+    # copy the input list to a buffer for FFTs
+    y=[]
+    i=0
+    while i < len(list):
+        y.append(float(list[i][1]))
+        i += 1
+    return y
+
+def autocorrelate(y):
+    ypad=[]
+    i=0
+    while i < len(y):
+        ypad.append(y[i])
+        i += 1
+    while i < len(y):
+        ypad.append(0.0)
+        i += 1
+    Y=np.fft.rfft(ypad)
+    i=0
+    while i < len(Y):
+        Y[i] *= np.conj(Y[i])
+        i += 1
+    yac = np.fft.irfft(Y)
+    end=np.floor(len(yac)/2)
+    yac = yac[0:end]
+    norm=yac[0]
+    i=0
+    while i < len(yac):
+        yac[i] /= norm
+        i += 1
+    return yac
+
 def main(argv):
 
-    usage="FIXME"
+    usage="./stationary -f <filename>"
 
     filename=""
 
@@ -34,7 +71,10 @@ def main(argv):
     for row in csvReader:
         a.append(row)
     
-    
+    y=ypart(a)
+    #print y
+    yac=autocorrelate(y)
+    print yac
 
 # The main program is called from here
 if __name__ == "__main__":
